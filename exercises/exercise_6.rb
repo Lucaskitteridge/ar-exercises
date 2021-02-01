1b30201df81e4ca23b069a268fe2825ace75f163
Lucaskitteridge <Lucas.benjamin.kitteridge@gmail.com>
2021-02-01 21:19:30 +0000

    exercise 6 and 7, trying extra

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,18 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Lucas", last_name: "Kitteridge", hourly_rate: 180)
+@store1.employees.create(first_name: "Sophie", last_name: "Ryder", hourly_rate: 60)
+@store1.employees.create(first_name: "Matt", last_name: "Morden", hourly_rate: 45)
+@store1.employees.create(first_name: "Morley", last_name: "Willoughby", hourly_rate: 40)
+@store2.employees.create(first_name: "Alex", last_name: "King", hourly_rate: 45)
+@store2.employees.create(first_name: "Krista", last_name: "Camilla", hourly_rate: 190)
+@store2.employees.create(first_name: "Zach", last_name: "Grant", hourly_rate: 85)
+@store2.employees.create(first_name: "Dylan", last_name: "Smith", hourly_rate: 132)
+@store2.employees.create(first_name: "Arbaaz", last_name: "Ghani", hourly_rate: 18)
+@store2.employees.create(first_name: "Jane", last_name: "Angela", hourly_rate: 58)
+@store2.employees.create(first_name: "Nicole", last_name: "Kuzmitch", hourly_rate: 148)
+@store2.employees.create(first_name: "Alex", last_name: "Sinicyn", hourly_rate: 71)
+@store2.employees.create(first_name: "Katie", last_name: "Weeks", hourly_rate: 156)
